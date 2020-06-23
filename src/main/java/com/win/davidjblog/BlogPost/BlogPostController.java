4158b01794461392e2ebf140589b7fa2a5265e40
David Juarez <djuar05@gmail.com>
2020-06-23 08:42:32 -0400

    controller file and index file

---
+++ src/main/java/com/win/davidjblog/BlogPost/BlogPostController.java
@@ -0,0 +1,15 @@
+package com.win.davidjblog.BlogPost;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BlogPostController {
+    
+    @GetMapping
+    public String index(BlogPost blogPost){
+        return "blogpost/index";
+    }
+
+    
+}
