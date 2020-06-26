a2f15c6807e7c4fb719991b31957c5bd90321768
David Juarez <djuar05@gmail.com>
2020-06-26 09:18:21 -0400

    create edit method with Pathvariable, Optional

--- src/main/java/com/win/davidjblog/BlogPost/BlogPostController.java
+++ src/main/java/com/win/davidjblog/BlogPost/BlogPostController.java
@@ -2,12 +2,16 @@ package com.win.davidjblog.BlogPost;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class BlogPostController {
@@ -48,6 +52,22 @@ public class BlogPostController {
         model.addAttribute("blogEntry", blogPost.getBlogEntry());
 
         return "blogpost/result";
+    }
+
+    // Similar to @PostMapping or @GetMapping, but allows for @PathVariable
+    @RequestMapping(value = "/blogposts/{id}", method = RequestMethod.GET)
+    // Spring takes whatever value is in {id} and passes it to our method params using @PathVariable
+    public String editPostWithId(@PathVariable Long id, BlogPost blogPost, Model model){
+        // findById() returns and Optional<T> which can be null, so we have to test
+        Optional<BlogPost> post = blogRepo.findById(id);
+        // Test if post acutally has anything in it
+        if(post.isPresent()){
+            //UnWrap the post from Optional shell
+            BlogPost actualPost = post.get();
+            model.addAttribute("blogPost", actualPost);
+        }
+
+        return "blogpost/edit";
     }
 
     
